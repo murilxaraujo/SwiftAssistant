da1a33f4bf62c78444ec87c0baca148c0cbc3ebf
Murilo Araujo <murilo@muriloaraujo.com>
2023-03-12 12:55:30 -0300

    Initial Commit

---
+++ XcodeGPT/XcodeGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGPTApp.swift
+//  XcodeGPT
+//
+//  Created by Murilo Araujo on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
